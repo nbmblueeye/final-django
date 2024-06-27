91cedd32c069b987d9e02e8407eb13a61821f661
nbmblueeye <nbmblueeye@gmail.com>
2024-06-27 12:30:01 -0400

    completed project

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -113,7 +113,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
     
 class Submission(models.Model):
